d154b7917453626f564eacc970b1726721fd0757
pravlearn <115153400+pravlearn@users.noreply.github.com>
2022-12-15 04:02:50 +0000

    Add files via upload

---
+++ HOL/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HOL2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            Models.Product p = new Models.Product();
+            return View(p.GetProducts());
+        }
+    }
+}
