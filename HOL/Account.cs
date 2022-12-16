a2f70956950111b1a2df9ac8eef2a8db82e8ccb5
pravlearn <115153400+pravlearn@users.noreply.github.com>
2022-12-16 04:30:39 +0000

    Add files via upload

---
+++ HOL/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HOL4.Models
+{
+    public class Account
+    {
+        [Key]
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+
+        public int AccountNumber { get; set; }
+
+        public string Name { get; set; }
+
+        public double CurrentBalance { get; set; }
+    }
+}
